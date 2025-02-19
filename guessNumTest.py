1aebba75d8c5a54f47dce7f99da3a5f984c1329e
surendar2011 <s.surendar11@gmail.com>
2025-02-19 16:36:33 +0530

    new comment

--- guessNumTest.py
+++ guessNumTest.py
@@ -3,6 +3,7 @@
 import random
 
 # assign a target num and guess num
+# used a two values to two variable in one line, random.randint => to target_num and 0 => guess_num
 target_num, guess_num = random.randint(1, 5), 0 
 
 # start a while loop until ENTER the guess number
